f11295d0bc7c49f2153dfc6fb6b5787b0dd74be4
Yuriy Grebenshchikov <grur.ura@mail.ru>
2023-08-05 04:53:35 +0500

    meta head updates

--- src/routes/tracks/TrackListTile.svelte
+++ src/routes/tracks/TrackListTile.svelte
@@ -10,28 +10,28 @@
 </script>
 
 <div class="track-tile py-4 flex sm:gap-5 lg:gap-10">
-    <a href="{url}" target="_blank">
-        <div class="flex-1 flex flex-col gap-1 justify-between">
-            <div class="text-md sm:text-xl"><b>{title}</b></div>
-            <div class="text-sm text-gray-400">{owner}</div>
-        </div>
-        <div class="flex flex-col justify-start sm:justify-center sm:flex-row gap-1 sm:gap-5 lg:gap-10">
-            <div class="flex flex-col items-end sm:items-center">
-                <div class="text-md">
-                    {date_created_at.toLocaleTimeString("ru-RU", {
-                        hour: '2-digit',
-                        minute: '2-digit',
-                    })}
-                </div>
-                <div class="text-sm text-gray-400">{date_created_at.toLocaleDateString("ru-RU", {
-                    year: 'numeric',
-                    month: 'numeric',
-                    day: 'numeric',
-                })}</div>
+    <div class="flex-1 flex flex-col gap-1 justify-between">
+        <div class="text-md sm:text-xl"><b>{title}</b></div>
+        <div class="text-sm text-gray-400">{owner}</div>
+    </div>
+    <div class="flex flex-col justify-start sm:justify-center sm:flex-row gap-1 sm:gap-5 lg:gap-10">
+        <div class="flex flex-col items-end sm:items-center">
+            <div class="text-md">
+                {date_created_at.toLocaleTimeString("ru-RU", {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                })}
             </div>
-            <div class="flex justify-end sm:justify-center">
+            <div class="text-sm text-gray-400">{date_created_at.toLocaleDateString("ru-RU", {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+            })}</div>
+        </div>
+        <div class="flex justify-end sm:justify-center">
+            <a href="{url}" target="_blank">
                 <img class="w-10 sm:w-14" src="{youtube_logo}" alt="YT Link">
-            </div>
+            </a>
         </div>
-    </a>
+    </div>
 </div>
