3738fd56690f787b64e33b90234e0369396b50c5
Yuriy Grebenshchikov <grur.ura@mail.ru>
2023-11-15 10:13:09 +0500

    added NAP

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -5,14 +5,27 @@
 <footer class="p-4 md:px-24">
     <div class="flex flex-col justify-center items-center">
         <Nav/>
-        <div class="mt-10">
-            <p>By <a class="text" href="https://github.com/bullbulk">bullbulk</a></p>
-        </div>
+        <address>
+            <div class="made-by">
+                <p>By <a class="text" href="https://github.com/bullbulk">bullbulk</a></p>
+            </div>
+            <div class="contact">
+                <p><a class="text" href="mailto:admin@bullbulk.ru">Contact admin</a></p>
+            </div>
+        </address>
     </div>
 </footer>
 
-<style>
-    footer a {
-        font-weight: bold;
+<style lang="scss">
+  footer a {
+    font-weight: bold;
+  }
+
+  address {
+    @apply mt-20 not-italic flex flex-col justify-center items-center;
+
+    & .contact * {
+      @apply mt-2 text-[--color-separator] text-xs;
     }
+  }
 </style>
